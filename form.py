5c40b62b4eb868f718700f8a5f9c261379cbaf2e
yang <ayang23@gmail.com>
2012-03-28 18:01:31 +0800

    developed a simple form function

--- form.py
+++ form.py
@@ -2,10 +2,11 @@ from tornado.util import ObjectDict
 
 
 class InputField(ObjectDict):
-	def __init__(self, name, type, value="", placeholder="", error="", help=""):
-		self["name"] = name
-		self["type"] = type
-		self["value"] = value
-		self["placeholder"] = placeholder
-		self["error"] = error
-		self["help"] = help
+    def __init__(self, name, type, label="", value="", placeholder="", error="", help=""):
+        self["name"] = name
+        self["type"] = type
+        self["label"] = label
+        self["value"] = value
+        self["placeholder"] = placeholder
+        self["error"] = error
+        self["help"] = help
--- handlers/accounts.py
+++ handlers/accounts.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tornado.web
 from models.topic import UserManager
 from handlers.base import BaseHandler
@@ -7,13 +8,14 @@ from form import InputField
 class SignupHandler(BaseHandler):
     def prepare(self):
         self.form = {
-            "email": InputField("email", "text"),
-            "password": InputField("password", "password"),
-            "password2": InputField("password2", "password"),
+            "name": InputField("name", "text", "昵称", help="选一个合适的昵称，可以使用您的名字"),
+            "email": InputField("email", "text", "电子邮件", help="请输入常用电子邮件，以便找回密码"),
+            "password": InputField("password", "password", "密码", help="密码长一点才是王道，话说8位以下都Out了"),
+            "password2": InputField("password2", "password", "密码(重复)", help="麻烦您再输一遍"),
         }
 
     def get(self):
-        self.render("accounts/signup.html")
+        self.render("accounts/signup.html", form=self.form)
 
     def post(self):
         email = self.get_argument('email', "")
@@ -22,4 +24,4 @@ class SignupHandler(BaseHandler):
         if password != password2:
             self.form["password2"].error = True
         self.form["email"].value = email
-        self.render("accounts/signup.html")
+        self.render("accounts/signup.html", form=self.form)
